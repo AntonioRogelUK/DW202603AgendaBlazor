1bd9913c22330ba985085800c49d1045687e6cb7
AntonioRogel <63703411+AntonioRogelUK@users.noreply.github.com>
2026-03-17 07:23:19 -0700

    Add files via upload

---
+++ AgendaWeb/Components/Pages/ContactoNuevo.razor.cs
@@ -0,0 +1,27 @@
+﻿using AgendaWeb.Data.DTOS.Contactos;
+using AgendaWeb.Services;
+using Microsoft.AspNetCore.Components;
+
+namespace AgendaWeb.Components.Pages
+{
+    public partial class ContactoNuevo
+    {
+        protected ContactoNuevoDto Contacto { get; set; } = new();
+
+        protected bool MensajeExito { get; set; } = false;
+
+        [Inject] protected NavigationManager Navigation { get; set; }
+        [Inject] protected ContactoServices ContactoServices { get; set; }
+
+        protected async Task GuardarContacto()
+        {
+            ContactoServices.Insertar(Contacto);
+
+            await Task.CompletedTask;
+        }
+        protected void Cancelar()
+        {
+            Navigation.NavigateTo("/contactos");
+        }
+    }
+}
---
+++ AgendaWeb/Data/DTOS/Contactos/ContactoNuevoDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgendaWeb.Data.DTOS.Contactos
+{
+    public class ContactoNuevoDto
+    {
+        [Required(ErrorMessage = "El campo Nombre es requerido")]
+        public string Nombre { get; set; }
+        
+       
+        public string Telefono { get; set; }
+
+        
+        
+        [Required(ErrorMessage = "El campo Email es requerido")]
+        public string Email { get; set; }
+    }
+}
